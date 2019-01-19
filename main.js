68d51f4414a683159f1d9516dfac00824fdfc6f8
sunziqiang <sunziqiang@wecash.net>
2019-01-19 17:57:48 +0800

    finish use a git repo as the post lib

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 import express from 'express'
 import handler from './src/handler'
-import fs from 'fs'
-
+import postRepo from './src/post_repo'
+import config from 'config'
 
 async function main() {
+    await init()
+
+    const projectPath = postRepo.getProjectPath(config.get('posts').git, config.get('posts').localPath)
+
     const app = express()
 
     app.set('views', './views');
     app.set('view engine', 'pug');
-    app.use('/static', express.static('public'));
-    app.use('/img', express.static('_post/img'));
+    app.use('/static', express.static('static'));
+    app.use('/static', express.static(`${projectPath}/static`));
 
     app.get('/', handler.index)
     app.get('/*', handler.getPost);
@@ -18,6 +22,15 @@ async function main() {
     app.listen(3000, function () {
         console.log("App is runing on port 3000!")
     })
+}
+
+
+async function init() {
+    console.info("Init posts resources...")
+    const postsConfig = config.get('posts')
+
+    await postRepo.getNewCommit(postsConfig.git, postsConfig.localPath)
+    console.info("Finish init posts resources.")
 }
 
 
